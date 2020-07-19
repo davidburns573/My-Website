1586b9687b949040120c173f5cd0b40d01da44b5
David Burns <davidburns573@gmail.com>
2020-07-19 15:34:52 -0500

    navbar animations

--- main.js
+++ main.js
@@ -13,3 +13,27 @@ function typeWriter() {
 function mailClick() {
     window.location.href='mailto:dburns37@gatech.edu';
 }
+
+function about() {
+    var element = document.getElementById("aboutline");
+    element.parentNode.removeChild(element);
+    document.getElementById("aboutgroup").appendChild(element);
+}
+
+function projects() {
+    var element = document.getElementById("projectsline");
+    element.parentNode.removeChild(element);
+    document.getElementById("projectsgroup").appendChild(element);
+}
+
+function involvement() {
+    var element = document.getElementById("involvementline");
+    element.parentNode.removeChild(element);
+    document.getElementById("involvementgroup").appendChild(element);
+}
+
+function contact() {
+    var element = document.getElementById("contactline");
+    element.parentNode.removeChild(element);
+    document.getElementById("contactgroup").appendChild(element);
+}
